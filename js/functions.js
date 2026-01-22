bcee882d36b06c1ddef08ff2004dc012037ec8ff
dmitry.buyan <dmlbuyan@gmail.com>
2026-01-22 13:09:21 +0400

    Функция для проверки длины строки

---
+++ js/functions.js
@@ -0,0 +1,6 @@
+// Task 1
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+
+checkStringLength('проверяемая строка', 20); // true
+checkStringLength('проверяемая строка', 18); // true
+checkStringLength('проверяемая строка', 10); // false
